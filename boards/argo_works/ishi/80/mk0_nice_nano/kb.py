c0e9b02eebea98c9ae7ad8023c15e74919cc1216
Jason Devadoss <jasonj2232@gmail.com>
2022-12-06 15:13:18 +0530

    Renamed directory and finished initial work on board.py file

--- boards/argo_works/ishi/80/mk0_nice_nano/kb.py
+++ boards/argo_works/ishi/80/mk0_nice_nano/kb.py
@@ -27,5 +27,11 @@ class KMKKeyboard(_KMKKeyboard):
         board.pins[6],
         board.pins[1]
     )
-    diode_orientation = DiodeOrientation.COLUMNS
-    i2c = board.I2C
+    diode_orientation = DiodeOrientation.COL2ROW
+    coord_mapping = [
+         9,  0, 10,  1,    11,  2, 12,  3, 13,  4, 14,  5, 15,  6, 16,  7, 17,  8,
+        27, 18, 28, 19,    29, 20, 30, 21, 31, 22, 32, 23, 33, 24, 34, 25, 35, 26,
+            36, 46, 37,    47, 38, 48, 39, 49, 40, 50, 41, 51, 42, 52, 43,     44,
+        63, 54, 64, 55,    65, 66, 57, 67, 58, 68, 59, 69, 60, 70, 61,         62,
+            72, 73,        74, 56, 75,             76,             77, 78, 79, 71,   80 
+    ]
